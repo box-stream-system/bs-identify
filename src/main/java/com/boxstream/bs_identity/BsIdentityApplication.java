9d624a6e49ba2d31d1da80720521a6e77fb0a348
tienphuckx <tienphuckx@gmail.com>
2025-01-06 14:15:31 +0700

    Init bs-identity

---
+++ src/main/java/com/boxstream/bs_identity/BsIdentityApplication.java
@@ -0,0 +1,13 @@
+package com.boxstream.bs_identity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsIdentityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsIdentityApplication.class, args);
+	}
+
+}
