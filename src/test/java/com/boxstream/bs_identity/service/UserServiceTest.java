3b50120cf944382e8af5890275b7126a5146105c
tienphuckx <tienphuckx@gmail.com>
2025-01-09 11:27:55 +0700

    impl: test case for user service

---
+++ src/test/java/com/boxstream/bs_identity/service/UserServiceTest.java
@@ -0,0 +1,112 @@
+package com.boxstream.bs_identity.service;
+
+import com.boxstream.bs_identity.dto.request.UserCreationRequest;
+import com.boxstream.bs_identity.dto.response.UserResponse;
+import com.boxstream.bs_identity.entity.User;
+import com.boxstream.bs_identity.exception.ApplicationException;
+import com.boxstream.bs_identity.exception.UsernameExistsException;
+import com.boxstream.bs_identity.repository.UserRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+
+@SpringBootTest
+@AutoConfigureMockMvc // for create a request to our test function
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockitoBean
+    private UserRepository userRepository;
+
+    private UserCreationRequest userCreationRequest;
+
+    private UserResponse userResponse;
+
+    private User user;
+
+    @BeforeEach
+    void initializeData() {
+        userCreationRequest = UserCreationRequest.builder()
+                .email("johnwick@gmail.com")
+                .dateOfBirth(LocalDate.of(1990, 1, 1))
+                .firstName("John")
+                .lastName("Wick")
+                .middleName("D")
+                .phone("+84 961463999")
+                .username("johnwick97")
+                .password("John@Wick97")
+                .build();
+
+        userResponse = UserResponse.builder()
+                .id("wrewwewwef") // fake id response
+                .email("johnwick@gmail.com")
+                .dateOfBirth("1990-1-1")
+                .firstName("John")
+                .lastName("Wick")
+                .middleName("D")
+                .phone("+84 961463999")
+                .username("johnwick97")
+                .build();
+
+        user = User.builder()
+                .id("wrewwewwef") // fake id response
+                .email("johnwick@gmail.com")
+                .dateOfBirth(LocalDate.of(1990, 1, 1))
+                .firstName("John")
+                .lastName("Wick")
+                .middleName("D")
+                .phone("+84 961463999")
+                .username("johnwick97")
+                .build();
+
+
+    }
+
+    @Test
+    void createUser_validRequest_success() {
+
+        // GIVEN: make deal
+        when(userRepository.existsByUsername(anyString())).thenReturn(false);
+        when(userRepository.save(any())).thenReturn(user);
+
+        // WHEN
+        var reponse = userService.createNewUser(userCreationRequest);
+
+        // THEN
+        Assertions.assertNotNull(reponse);
+        Assertions.assertEquals(userResponse.getFirstName(), reponse.getFirstName());
+        Assertions.assertEquals(userResponse.getLastName(), reponse.getLastName());
+        Assertions.assertEquals(userResponse.getEmail(), reponse.getEmail());
+        Assertions.assertEquals(userResponse.getPhone(), reponse.getPhone());
+        Assertions.assertEquals(userResponse.getUsername(), reponse.getUsername());
+        Assertions.assertEquals(userResponse.getId(), reponse.getId());
+
+    }
+
+    @Test
+    void createUser_userExists_success() {
+
+        // GIVEN: make deal
+        when(userRepository.existsByUsername(anyString())).thenReturn(true);
+
+        // WHEN
+        var exception = Assertions
+                .assertThrows(UsernameExistsException.class, () -> userService.createNewUser(userCreationRequest));
+
+        // THEN
+        Assertions.assertEquals(exception.getErrorCode().getCode(), 1004);
+        Assertions.assertEquals(exception.getMessage(), "Username already exists");
+    }
+}
